e791cfe3d42375407568d5696137e9bd3ed69912
gael p <gael.parra@epsi.fr>
2022-01-12 11:33:24 +0100

    modification fonction de clignotement

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,12 +1,26 @@
 #include <Arduino.h>
 
+void OnLed(){
+PORTB = PORTB | (1<<5);
+}
+
+void OffLed(){
+PORTB = PORTB &~(1<<5);
+}
+
+void ledSetup(){
+  DDRB = DDRB | (1<<5);
+}
+
 void setup() {
- pinMode(13, OUTPUT);
+  // put your setup code here, to run once:
+  ledSetup();
 }
 
 void loop() {
-  digitalWrite(13, HIGH); 
-  delay(1000); 
-  digitalWrite(13, LOW); 
+  // put your main code here, to run repeatedly:
+  OnLed();
+  delay(1000);
+  OffLed();
   delay(1000);
 }
