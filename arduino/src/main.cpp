e9cfa6d9e9460a14a16b835a75d33debb4e78573
gael p <gael.parra@epsi.fr>
2022-01-13 15:54:23 +0100

    add function timer

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -16,13 +16,23 @@ void ledSetup(){
   DDRB = DDRB | (1<<5);
 }
 
+void timerSetup(){
+  TCCR1A = 0;
+  TCCR1B = (1 << WGM12) | (1 << CS12) | (1 << CS10);
+  OCR1A = 15625;
+  TCNT1 = 0;
+  TIMSK1 = (1 << OCIE1A);
+}
+
+ISR(TIMER1_COMPA_vect){
+  toggle();
+}
+
 void setup() {
   // put your setup code here, to run once:
   ledSetup();
+  timerSetup();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  toggle();
-  delay(1000);
 }
